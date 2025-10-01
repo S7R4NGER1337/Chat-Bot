9d6e746f1900eba44c2a1e695f3c3f377effbf2a
S7R4NGER3301 <nikolakoew@gmail.com>
2025-10-01 19:22:03 +0300

    adding chat bot name and wrapping it with status dot

--- src/components/ChatBot/Header.js
+++ src/components/ChatBot/Header.js
@@ -3,7 +3,10 @@ import styles from "./header.module.css";
 export default function Header() {
   return (
     <div className={styles.headerContainer}>
-      <div className={styles.statusDot}></div>
+      <div className={styles.headerInfo}>
+        <div className={styles.statusDot}></div>
+        <p className={styles.headerTitle}>chat-bot</p>
+      </div>
       <div className={styles.headerActions}>
         <p className={styles.headerAction}>-</p>
         <p className={styles.headerAction}>x</p>
