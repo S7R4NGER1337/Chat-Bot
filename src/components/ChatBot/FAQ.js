8f93f5c1f48f8a4eb7973bc0c5da7a20d8b19538
S7R4NGER3301 <nikolakoew@gmail.com>
2025-10-02 19:49:10 +0300

    creating FAQ file and setting 3 questions

---
+++ src/components/ChatBot/FAQ.js
@@ -0,0 +1,15 @@
+export const FAQ = [
+    {
+        question: 'Do you ship internationally?',
+        answer: 'Yes we do ship internationally'
+    },
+    {
+        question: 'What are your shipping options?',
+        answer: 'We have many shipping options.'
+    },
+    {
+        question: 'How do I track my order?',
+        answer: 'You can use our built in tracker.'
+    },
+
+]
