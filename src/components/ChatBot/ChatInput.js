8386df277911350cf313ab7cb76590a96e185541
S7R4NGER3301 <nikolakoew@gmail.com>
2025-10-02 19:43:06 +0300

    handling the input

--- src/components/ChatBot/ChatInput.js
+++ src/components/ChatBot/ChatInput.js
@@ -1,9 +1,12 @@
+import { useState } from "react";
 import styles from "./chatInput.module.css";
 
 export default function ChatInput() {
+    const [message, setMessage] = useState('')
+
   return (
     <div className={styles.chatInputContainer}>
-      <input name="message" className={styles.chatInput} type="text" placeholder="Type a message" />
+      <input value={message} onChange={(e) => setMessage(e.target.value)} name="message" className={styles.chatInput} type="text" placeholder="Type a message" />
       <img className={styles.chatInputBtn}  src="/paper-plane-regular-full.svg" alt="sendMessage" />
     </div>
   );
