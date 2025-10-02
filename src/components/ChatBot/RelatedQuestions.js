1ddc7232bfe3ccd93ed53848f4279228f2439ecd
S7R4NGER3301 <nikolakoew@gmail.com>
2025-10-02 20:25:46 +0300

    rendering correct questions

--- src/components/ChatBot/RelatedQuestions.js
+++ src/components/ChatBot/RelatedQuestions.js
@@ -3,11 +3,11 @@ import styles from "./relatedQuestions.module.css";
 export default function RelatedQuestions({ options }) {
   return (
     <div className={styles.relatedQuestionsContainer}>
-      <h1 className={styles.relatedQuestionsTitle}>Related questions</h1>
+      <h1 className={styles.relatedQuestionsTitle}>Related questions: </h1>
       <div className={styles.questionsContainer}>
         {options.map((option) => (
-          <p className={styles.questionOption} key={option}>
-            {option}
+          <p className={styles.questionOption} key={option.question}>
+            {option.question}
           </p>
         ))}
       </div>
