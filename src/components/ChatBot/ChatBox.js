6e3326c8a2ddd0ef9a2876b86f1a83c6b268390f
S7R4NGER3301 <nikolakoew@gmail.com>
2025-10-02 19:22:48 +0300

    rendering messages dynamically with static data

--- src/components/ChatBot/ChatBox.js
+++ src/components/ChatBot/ChatBox.js
@@ -1,15 +1,31 @@
+import { useState } from "react";
 import styles from "./chatBox.module.css";
 import ChatMessage from "./ChatMessage";
 import Header from "./Navigation";
 
 export default function ChatBox() {
+  const [messages, setMessages] = useState([
+    {
+      text: "Test",
+      type: "user",
+    },
+    {
+      text: "Test2",
+      type: "bot",
+    },
+    {
+      text: "Test3",
+      type: "user",
+    },
+  ]);
+
   return (
     <div className={styles.chatBoxContainer}>
       <Header />
       <div className={styles.messagesContainer}>
-        <ChatMessage text={'Test'} type={'user'}/>
-        <ChatMessage text={'Test2'} type={'bot'}/>
-        <ChatMessage text={'Test2'} type={'user'}/>
+        {messages.map((message) => (
+          <ChatMessage text={message.text} type={message.type} />
+        ))}
       </div>
     </div>
   );
