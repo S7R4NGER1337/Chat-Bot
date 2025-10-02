190175dfdb99c00ddb1c5590fa35b0726ded65b5
S7R4NGER3301 <nikolakoew@gmail.com>
2025-10-02 19:09:07 +0300

    receiving props in chat message and rendering message text with correct style

--- src/components/ChatBot/ChatMessage.js
+++ src/components/ChatBot/ChatMessage.js
@@ -1,5 +1,13 @@
-import styles from './chatMessage.module.css'
+import styles from "./chatMessage.module.css";
 
-export default function ChatMessage(params) {
-    return <h1>Test</h1>
+export default function ChatMessage({ text, type }) {
+  return (
+    <p
+      className={`${styles.chatMessage} ${
+        type === "user" ? styles.userMessage : styles.botMessage
+      }`}
+    >
+      {text}
+    </p>
+  );
 }
