0c75c126d3348506dd54f9e6bc7537d6f0bfb2eb
S7R4NGER3301 <nikolakoew@gmail.com>
2025-10-02 19:41:23 +0300

    changing paper image

--- src/components/ChatBot/ChatInput.js
+++ src/components/ChatBot/ChatInput.js
@@ -3,8 +3,8 @@ import styles from "./chatInput.module.css";
 export default function ChatInput() {
   return (
     <div className={styles.chatInputContainer}>
-      <input type="text" placeholder="Type a message" />
-      <img src="/paper-plane-solid-full.svg" alt="sendMessage" />
+      <input name="message" className={styles.chatInput} type="text" placeholder="Type a message" />
+      <img className={styles.chatInputBtn}  src="/paper-plane-regular-full.svg" alt="sendMessage" />
     </div>
   );
 }
