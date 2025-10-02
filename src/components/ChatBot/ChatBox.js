7a774e7b5a622e231a77e9924691358a86860b9d
S7R4NGER3301 <nikolakoew@gmail.com>
2025-10-02 19:11:55 +0300

    adding 3 static messages and wrapping them in a div

--- src/components/ChatBot/ChatBox.js
+++ src/components/ChatBot/ChatBox.js
@@ -1,10 +1,16 @@
 import styles from "./chatBox.module.css";
+import ChatMessage from "./ChatMessage";
 import Header from "./Navigation";
 
 export default function ChatBox() {
   return (
     <div className={styles.chatBoxContainer}>
       <Header />
+      <div className={styles.messagesContainer}>
+        <ChatMessage text={'Test'} type={'user'}/>
+        <ChatMessage text={'Test2'} type={'bot'}/>
+        <ChatMessage text={'Test2'} type={'user'}/>
+      </div>
     </div>
   );
 }
